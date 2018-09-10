1cbee1f5114b5498b0d0d6b311b067f9c5b2c954
chenkunlia <chenkunlia@163.com>
2018-09-10 12:32:38 +0800

    dj-03:router 路由划分

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+// import VueRouter from 'vue-router'
+import router from './router/admin.js'
+// import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/style.css'
 Vue.config.productionTip = false
-
+// Vue.use(VueRouter)
+Vue.use(ElementUI)
+// const router = new VueRouter({admin})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
