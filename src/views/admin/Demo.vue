190951998e54222dcf204aaa9b50c0cb4742e423
chenkunlia <chenkunlia@163.com>
2018-09-11 16:34:35 +0800

    views added

---
+++ src/views/admin/Demo.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="hello">
+   <h3>{{ msg }}</h3>
+   <div class="block">
+    <span class="demonstration">默认</span>
+    <el-date-picker
+      v-model="value1"
+      type="datetime"
+      placeholder="选择日期时间">
+    </el-date-picker>
+  </div>
+  <div class="block">
+    <span class="demonstration">带快捷选项</span>
+    <el-date-picker
+      v-model="value2"
+      type="datetime"
+      placeholder="选择日期时间"
+      align="right"
+      :picker-options="pickerOptions1">
+    </el-date-picker>
+  </div>
+  <div class="block">
+    <span class="demonstration">设置默认时间</span>
+    <el-date-picker
+      v-model="value3"
+      type="datetime"
+      placeholder="选择日期时间"
+      default-time="12:00:00">
+    </el-date-picker>
+  </div>
+  </div>
+</template>
+
+<script>
+import { Cell, Group } from 'vux'
+export default {
+  name: 'Demo',
+  components: {
+      Cell,
+      Group
+  },
+  data () {
+       return {
+        pickerOptions1: {
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+        value2: '',
+        value3: '',
+        msg: 'Welcome to Your Vue.js App'
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
