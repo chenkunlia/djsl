f6682c62b7a8d3bee43367e5d968405126f3c3db
chenkunlia <chenkunlia@163.com>
2018-09-11 16:39:26 +0800

    config added

--- src/main.js
+++ src/main.js
@@ -3,18 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 // import VueRouter from 'vue-router'
-import router from './router/admin.js'
+import router from './router/admin'
 // import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/style.css'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import i18n from './lang'
 Vue.config.productionTip = false
 // Vue.use(VueRouter)
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.use(ElementUI, {
+  size: 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
 // const router = new VueRouter({admin})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
