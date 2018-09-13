2d9bd1c3d1423963afff36339ef504156647c2c5
chenkunlia <chenkunlia@163.com>
2018-09-13 11:48:16 +0800

    admin

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -5,9 +5,7 @@ import App from './App'
 // import VueRouter from 'vue-router'
 import router from '@/router/admin.router'
 // import router from './router'
-import ElementUI from 'element-ui'
 
-import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.scss' // global css
 import i18n from '@/lang'
 import store from '@/store'
@@ -17,12 +15,6 @@ import '@/errorLog' // error log
 import '@/permission' // permission control
 import * as filters from '@/filters' // global filters
 Vue.config.productionTip = false
-// Vue.use(VueRouter)
-// Vue.use(ElementUI)
-Vue.use(ElementUI, {
-  size: 'medium', // set element-ui default size
-  i18n: (key, value) => i18n.t(key, value)
-})
 
 // register global utility filters.
 Object.keys(filters).forEach(key => {
